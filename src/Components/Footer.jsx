bc82c81697ba328d615b29d16a2d873b9eff0c12
hamzaarif12345 <102205285+hamzaarif12345@users.noreply.github.com>
2023-10-08 14:56:12 +0530

    commit 4

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      
-      <p>Developed by <a href="https://hamzaarif12345.github.io/sample2/portfolio.html">Hamza Arif</a></p>
+        <span>Created By Hamza Arif | <span class="far fa-copyright"></span> All rights reserved.</span>
     </footer>
   );
 };
